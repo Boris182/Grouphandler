5c368b8ae5862dc0a1723fbb957d6bda127e2d52
Boris182 <martinachermann@msn.com>
2023-01-04 23:29:11 +0100

    Neuer Commit

--- sub.py
+++ sub.py
@@ -1,7 +1,10 @@
 import random
-import time
 
+import time
 from paho.mqtt import client as mqtt_client
+from time import sleep
+from threading import Thread
+
 
 broker = '192.168.70.91'
 port = 1883
@@ -9,18 +12,27 @@ topic = "test/mqtt"
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 
 
-def connect_mqtt():
+
+def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Connected to MQTT Broker!")
         else:
             print("Failed to connect, return code %d\n", rc)
-    # Set Connecting Client ID
+
     client = mqtt_client.Client(client_id)
-    # client.username_pw_set(username, password)
+    #client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
     return client
+
+
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
 
 def publish(client):
      msg_count = 0
@@ -38,8 +50,17 @@ def publish(client):
 
 def run():
     client = connect_mqtt()
-    client.loop_start()
+    subscribe(client)
+    client.loop_forever()
+
+def run2():
+    client = connect_mqtt()
     publish(client)
 
 if __name__ == '__main__':
-    run()
+    thread1 = Thread(target=run)
+    thread2 = Thread(target=run2)
+    thread1.start()
+    thread2.start()
+    print("Test")
+    print("Test2")
